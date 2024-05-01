5072699c1c112702a2fb91bbe5b8e61f5d1af01c
piyushphutela15 <122336044+piyushphutela15@users.noreply.github.com>
2024-05-01 13:09:55 +0530

    Add files via upload

---
+++ E comerce(front end)/index.js
@@ -0,0 +1,19 @@
+//swiper
+var swiper = new Swiper(".home", {
+    spaceBetween: 30,
+    centeredSlides: true,
+    navigation: {
+    nextEl: ".swiper-button-next",
+    prevel: ".swiper-button-prev",
+    },
+    });
+    let menu = document.querySelector('#menu-icon');
+    let navbar = document.querySelector('.navbar');
+    menu.onclick = () => {
+        menu.classList.toggle('bx-x');
+        navbar.classList.toggle('active');
+    }
+    window.onscroll = () => {
+        menu.classList.remove('bx-x');
+        navbar.classList.remove('active');   
+    }
